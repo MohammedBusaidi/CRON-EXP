9429d6108fece50ff9719c425912ceebf8cda0eb
Mohammed <moody.busaidi99@gmail.com>
2023-05-30 12:13:55 +0400

    first question

---
+++ src/main/java/com/Cron/CRON/Schedule.java
@@ -0,0 +1,13 @@
+package com.Cron.CRON;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Schedule {
+    @Scheduled(cron = "0 t " +
+            "/5 * * * * ")
+    public void scheduleTask() {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/Cron/CRON/CronApplicationTests.java
@@ -0,0 +1,14 @@
+package com.Cron.CRON;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootTest
+class CronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
