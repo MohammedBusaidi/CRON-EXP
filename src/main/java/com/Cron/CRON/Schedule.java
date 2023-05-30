3efa16d4f9eacd525e7d36daa8cbaf9effa579a3
Mohammed <moody.busaidi99@gmail.com>
2023-05-30 12:30:53 +0400

    14th Q

--- src/main/java/com/Cron/CRON/Schedule.java
+++ src/main/java/com/Cron/CRON/Schedule.java
@@ -4,7 +4,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 @Component
 public class Schedule {
-    @Scheduled(cron = "0 0 18 * * 0,1,2,3,4,5,6")
+    @Scheduled(cron = "0 */5 * * * 0-4")
+    @Scheduled(cron = "0 */15 * * * 5-6")
     public void scheduleTask() {
         System.out.println("Hello World");
     }
